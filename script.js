fca5b96a7d12cb8f606accfaedd5dc60749a092c
yenohp <devinanderson286@gmail.com>
2022-05-27 20:39:11 -0700

    The submit button actually pushes the new book object to the array

--- script.js
+++ script.js
@@ -2,17 +2,30 @@ const addButton = document.querySelector('#add-large');
 const body = document.querySelector('body');
 const modal = document.querySelector('.modal');
 const mainContent = document.querySelector('.main-content');
+const closeButton = document.querySelector('.close-button');
+const inputs = Array.from(document.querySelectorAll('input'));
+const form = document.querySelector('form');
 
 const collection = [];
 
 addButton.addEventListener('click', (e) => {
-    // Can only click once 
-    e.target.disabled = 'true';
-    console.log('click');
     modal.classList.add('show-modal');
 
 })
+closeButton.addEventListener('click', (e) => {
+    modal.classList.remove('show-modal');
+    // Clear inputs
+    clearForm();
+})
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let title, author, pages, read;
+    [title, author, pages, read] = [e.target[0].value, e.target[1].value, e.target[2].value, e.target[3].checked];
+    addBook(title, author, pages, read);
+    modal.classList.remove('show-modal');
+    clearForm();
+})
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -29,11 +42,11 @@ Book.prototype.toggleRead = function () {
     else this.read = true;
 
 }
-function addBook() {
-    // submit/cancel buttons
-    // if either is pressed, remove the pop-up form
-    // if submitted, add the book to the collection array && create a visual representation
-    // if canceled, disregard all inputs
+function addBook(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+    // Check if duplicate
+    // if(!checkForDuplicate(book));
+    collection.push(book);
 }
 
 function removeBook() {
@@ -48,7 +61,12 @@ function displayCollection() {
     }
 }
 
-
+function clearForm() {
+    for (let input of inputs) {
+        if (input.type == 'text' || input.type == 'number') input.value = '';
+        else input.checked = false;
+    }
+}
 // Maths for calculating position
 // Row = if(arr.length > 6) { compare to multiples of 6}
 // Column = 
